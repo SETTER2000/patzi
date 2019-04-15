bfffdc60cc8b86a654e123c6944d84d3ccddd55a
SETTER2000 <lphp@mail.ru>
2019-04-15 03:09:04 +0300

    Бэмс!

--- assets/dependencies/main.js
+++ assets/dependencies/main.js
@@ -486,18 +486,18 @@ function initMap() {
 // external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
 // init Masonry
-// const grid = document.querySelector('.grid');
-//
-// const msnry = new Masonry( grid, {
-//   itemSelector: '.grid-item',
-//   columnWidth: '.grid-sizer',
-//   percentPosition: true
-// });
-//
-// imagesLoaded( grid ).on( 'progress', function() {
-//   // layout Masonry after each image loads
-//   msnry.layout();
-// });
+var grid = document.querySelector('.grid');
+
+var msnry = new Masonry( grid, {
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});
+
+imagesLoaded( grid ).on( 'progress', function() {
+  // layout Masonry after each image loads
+  msnry.layout();
+});
 
 
 
