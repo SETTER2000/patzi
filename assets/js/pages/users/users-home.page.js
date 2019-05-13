da057bbc21b0f91647c8e1222403770e33240f75
SETTER2000 <lphp@mail.ru>
2019-05-14 01:53:24 +0300

    Search ...

--- assets/js/pages/users/users-home.page.js
+++ assets/js/pages/users/users-home.page.js
@@ -3,7 +3,36 @@ parasails.registerPage('users-home', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
-    //…
+    cols: [
+      {
+        label: 'Name222',
+        field: 'name',
+      },
+      {
+        label: 'Age',
+        field: 'age',
+        type: 'number',
+      },
+      {
+        label: 'Created On',
+        field: 'createdAt',
+        type: 'date',
+        dateInputFormat: 'YYYY-MM-DD',
+        dateOutputFormat: 'MMM Do YY',
+      },
+      {
+        label: 'Percent',
+        field: 'score',
+        type: 'percentage',
+      }],
+    rws: [
+      {id: 1, name: "John555", age: 20, createdAt: '201-10-31:9: 35 am', score: 0.03343},
+      {id: 2, name: "Jane", age: 24, createdAt: '2011-10-31', score: 0.03343},
+      {id: 3, name: "Susan", age: 16, createdAt: '2011-10-30', score: 0.03343},
+      {id: 4, name: "Chris", age: 55, createdAt: '2011-10-11', score: 0.05343},
+      {id: 5, name: "Dan", age: 40, createdAt: '2011-10-21', score: 0.03343},
+      {id: 6, name: "John", age: 20, createdAt: '2011-10-31', score: 0.03343},
+    ]
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -14,7 +43,7 @@ parasails.registerPage('users-home', {
     _.extend(this, SAILS_LOCALS);
   },
   mounted: async function() {
-    //…
+
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
