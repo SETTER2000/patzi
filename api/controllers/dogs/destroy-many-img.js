dceed0b3e717512decffc2e76902858258a1f2f4
SETTER2000 <lphp@mail.ru>
2019-09-17 06:57:27 +0300

    Ass

---
+++ api/controllers/dogs/destroy-many-img.js
@@ -0,0 +1,67 @@
+module.exports = {
+
+
+  friendlyName: 'Destroy many img',
+
+
+  description: '',
+
+
+  inputs: {
+    id: {
+      type: 'string',
+      description: `Идентификатор собаки с которой работает действие.`
+    },
+    removeImage: {
+      type: 'ref',
+      description: `Массив с ID о файлах-картинках, которые должны быть удалены.`
+    },
+
+  },
+
+
+  exits: {
+    success: {
+      outputDescription: 'Information about the newly created record.',
+      // Устанавливаем выходной тип данных. Хорошая практика для документирования кода.
+      outputType: {
+        id: 'number',
+        imageSrc: 'string'
+      },
+    },
+    badRequest: {
+      description: 'No image upload was provided.',
+      responseType: 'badRequest'
+    },
+
+    dogAlreadyInUse: {
+      statusCode: 409,
+      description: 'The specified dog name is already in use.',
+    },
+
+    dogAlreadyInUseRU: {
+      statusCode: 409,
+      description: 'Указанное имя собаки уже используется.',
+    },
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    const req = this.req;
+    if (!req.isSocket) {
+      throw 'badRequest';
+    }
+    let dog = await Dog.findOne(inputs.id);
+    console.log('IN:: ', inputs.removeImage);
+    let removeImage = _.remove(dog.images, img => _.indexOf(inputs.removeImage, img.id) > -1);
+    console.log('removeImage::: ' , removeImage);
+    console.log('dog.images::: ' , dog.images);
+
+    let updateDog = await Dog.updateOne({id: inputs.id})
+      .set({images:dog.images});
+    return exits.success();
+  }
+
+
+};
---
+++ api/controllers/dogs/update-description-img.js
@@ -0,0 +1,80 @@
+module.exports = {
+
+
+  friendlyName: 'Update description img',
+
+
+  description: '',
+
+
+  inputs: {
+    id: {
+      type: 'string',
+      description: `Идентификатор собаки с которой работает действие.`
+    },
+    photoId: {
+      type: 'string',
+      description: `Идентификатор фото для которой предназначено описание.`
+    },
+    description: {
+      type: 'string',
+      maxLength: 300,
+      description: `Описание файла картинки.`
+    },
+
+  },
+
+
+  exits: {
+    success: {
+      outputDescription: 'Information about the newly created record.',
+      // Устанавливаем выходной тип данных. Хорошая практика для документирования кода.
+      outputType: {
+        id: 'number',
+        imageSrc: 'string'
+      },
+    },
+    badRequest: {
+      description: 'No image upload was provided.',
+      responseType: 'badRequest'
+    },
+
+    dogAlreadyInUse: {
+      statusCode: 409,
+      description: 'The specified dog name is already in use.',
+    },
+
+    dogAlreadyInUseRU: {
+      statusCode: 409,
+      description: 'Указанное имя собаки уже используется.',
+    },
+  },
+
+
+  fn: async function (inputs, exits) {
+    const req = this.req;
+    if (!req.isSocket) {
+      throw 'badRequest';
+    }
+
+    console.log('IDD: ' , inputs.id);
+    console.log('inputs.photoId: ' ,inputs.photoId);
+    let dog = await Dog.findOne(inputs.id);
+    _.each(dog.images, img => {
+      img.id === inputs.photoId ? img.description = inputs.description : '';
+    });
+    // console.log('IN:: ', inputs.removeImage);
+    // let removeImage = _.remove(dog.images, img => _.indexOf(inputs.removeImage, img.id) > -1);
+    // console.log('removeImage::: ', removeImage);
+    // console.log('dog.images::: ', dog.images);
+
+    let updateDog = await Dog.updateOne({id: inputs.id})
+      .set({images: dog.images});
+
+
+    return exits.success();
+
+  }
+
+
+};
