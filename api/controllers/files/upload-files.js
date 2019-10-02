739302684d19b772519b775de8bb4559fbfeae05
SETTER <info@lphp.ru>
2019-10-02 12:17:55 +0300

    Jimp example

--- api/controllers/files/upload-files.js
+++ api/controllers/files/upload-files.js
@@ -37,6 +37,7 @@ module.exports = {
   fn: async function (inputs, exits) {
     const req = this.req;
     const fs = require('fs');
+    const Jimp = require('jimp');
     const sharp = require('sharp');
     const gm = require('gm')
       , resizeX = 1424
@@ -48,9 +49,8 @@ module.exports = {
     let info = '';
 
     console.log("inputs.file::", inputs.file);
-    console.log("req.file('file')::",req.file('file'));
+    console.log("req.file('file')::", req.file('file'));
     // console.log("isBuffer::",_.isBuffer(req.file('file')));
-
 
 
     // const roundedCornerResizer =
@@ -68,7 +68,7 @@ module.exports = {
 
 
     info = await sails.upload(inputs.file);
-    console.log('INFO UPLOAD::: ' , info);
+    console.log('INFO UPLOAD::: ', info);
 
 
     let fd = _.pluck(info, 'fd')[0];
@@ -77,36 +77,51 @@ module.exports = {
 // console.log("DIRNAME2:::: " , fs.readFileSync(__dirname+"/../../../assets/fonts/OpenSans-Light.ttf"));
 // console.log("DIRNAME3:::: " , sails.getBaseurl());
 
-
-    gm(fd)
-    // .flip()
-    // .magnify()
-    // .rotate('green', 45)
-    // .blur(7, 3)
-    //   .resizeExact(resizeX,resizeY)
-
-      .autoOrient()
-      // .noProfile()
-      .bitdepth(16)
-      // Например, чтобы увеличить яркость цвета на 10% и уменьшить насыщенность
-      // цвета на 10% и оставить без изменений оттенок, используйте: -modulate 110,90
-      .modulate(110,90)
-      .resize(resizeX, resizeY)
-      // .gravity(gravity) // Какую область оставить в обрезке
-      // .crop(resizeX, resizeY)
-      // .edge(3)
-      // .stroke("#FFFFFF")
-      // .drawCircle(10, 10, 20, 10)
-      // .font( fs.readFileSync(__dirname+"/../../../assets/fonts/OpenSans-Light.ttf"), 12)
-      // .drawText(50, resizeY - 20, "www.poaleell.com")
-      .compress('JPEG')
-      .quality(Q) // качество сжатия изображения
-       // глубина цвета
-      // .sampling-factor("2x1")
-
-      .write(fd, function (err) {
-        if (err) console.log('Error loading images in upload-files::: ', err);
+    Jimp.read(fd)
+      .then(resizePhoto => {
+        return resizePhoto
+        // .resize(Jimp.AUTO, 256) // resize
+          .quality(40) // set JPEG quality
+          .cover(resizeX, resizeY)
+          .normalize()
+          // .greyscale() // set greyscale
+          .write(fd); // save
+      })
+      .catch(err => {
+        console.error(err);
       });
+    /*
+
+        gm(fd)
+        // .flip()
+        // .magnify()
+        // .rotate('green', 45)
+        // .blur(7, 3)
+        //   .resizeExact(resizeX,resizeY)
+
+          .autoOrient()
+          // .noProfile()
+          .bitdepth(16)
+          // Например, чтобы увеличить яркость цвета на 10% и уменьшить насыщенность
+          // цвета на 10% и оставить без изменений оттенок, используйте: -modulate 110,90
+          .modulate(110, 90)
+          .resize(resizeX, resizeY)
+          // .gravity(gravity) // Какую область оставить в обрезке
+          // .crop(resizeX, resizeY)
+          // .edge(3)
+          // .stroke("#FFFFFF")
+          // .drawCircle(10, 10, 20, 10)
+          // .font( fs.readFileSync(__dirname+"/../../../assets/fonts/OpenSans-Light.ttf"), 12)
+          // .drawText(50, resizeY - 20, "www.poaleell.com")
+          .compress('JPEG')
+          .quality(Q) // качество сжатия изображения
+          // глубина цвета
+          // .sampling-factor("2x1")
+
+          .write(fd, function (err) {
+            if (err) console.log('Error loading images in upload-files::: ', err);
+          });
+    */
 
 
     // sharp(fd)
