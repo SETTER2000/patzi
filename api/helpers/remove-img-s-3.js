f16986bf97f3da702227695ba77321aeed58918d
SETTER <info@lphp.ru>
2019-10-09 16:05:01 +0300

    s3 remove

---
+++ api/helpers/remove-img-s-3.js
@@ -0,0 +1,62 @@
+module.exports = {
+
+
+  friendlyName: 'Remove img s 3',
+
+
+  description: '',
+
+
+  inputs: {
+    removeImage: {
+      type: 'ref',
+      description: `Коллекция картинок, которые следует удалить из S3`,
+      example: `
+      [ { fd: '38155c8d-cd57-4877-964b-a2de849258f7.jpg', 
+          size: 34964, 
+          type: 'image/jpeg', 
+          name: '2_2.jpg', 
+          id: '38155c8d-cd57-4877-964b-a2de849258f7', 
+          description: '', 
+          dateTaken: '' } ]
+      `
+    },
+
+    bucket: {
+      type: 'string',
+      description: `Наименование корзины в сервисе S3 из которой удалить картинки.`
+    }
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'All done.',
+    },
+
+  },
+
+
+  fn: async function (inputs) {
+    if (sails.config.environment === 'production' && inputs.removeImage) {
+      // const skipper = require('skipper-s3')(
+      const skp = require('@setter/skp')(
+        {
+          key: sails.config.uploads.key,
+          bucket: sails.config.uploads.bucket || process.env.S3_BUCKET || inputs.bucket,
+          region: sails.config.uploads.region,
+          secret: sails.config.uploads.secret
+        }
+      );
+
+      _.each(removeImage, img => {
+        skp.rm(img.fd, (err, res) => {
+          if (err) console.log(`Ошибка при удаление файла ${img.fd}::`, err);
+          console.log('Response::: ', res);
+        });
+      });
+    }
+  }
+};
+
