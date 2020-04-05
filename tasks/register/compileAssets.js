38d540b9128cd3ed7e756ef55222cd72d75d2f1c
SETTER2000 <lphp@mail.ru>
2020-04-06 01:59:30 +0300

    Fix bug

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -10,9 +10,9 @@
 module.exports = function(grunt) {
   grunt.registerTask('compileAssets', [
     'clean:dev',
-    'jst:dev',
+    // 'jst:dev',
     'less:dev',
     'copy:dev',
-    'coffee:dev'
+    // 'coffee:dev'
   ]);
 };
