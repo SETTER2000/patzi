e2f5b98d0a6933cf92498a9f2b22f517da4be887
SETTER2000 <lphp@mail.ru>
2020-04-06 02:22:51 +0300

    Fixes

--- tasks/register/syncAssets.js
+++ tasks/register/syncAssets.js
@@ -9,9 +9,9 @@
  */
 module.exports = function(grunt) {
   grunt.registerTask('syncAssets', [
-    'jst:dev',
+    // 'jst:dev',
     'less:dev',
     'sync:dev',
-    'coffee:dev'
+    // 'coffee:dev'
   ]);
 };
