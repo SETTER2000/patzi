1931291abd73ee4852e17826355e47bb1a3f308c
SETTER <info@lphp.ru>
2020-07-31 14:55:21 +0300

    Add sl

--- assets/js/components/post/patzi-posts-item.component.js
+++ assets/js/components/post/patzi-posts-item.component.js
@@ -43,11 +43,13 @@ parasails.registerComponent('patziPostsItem', {
                             <div  class="u-container-layout u-valign-top-md u-valign-top-sm u-valign-top-xl u-valign-top-xs u-container-layout-1">
                                 <div class="u-align-left u-border-5 u-border-palette-1-base u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-shape u-shape-1"></div>
 
-                                <img v-if="post.images && (post.images.length>0)" class="u-expand-resize u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-image u-image-1 lazyload"
+                                <img ref="wrapper" v-if="post.images && (post.images.length>0)" 
+                                class="u-expand-resize u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-image u-image-1 lazyload"
                                      :data-src="post.images[0].imageSrc" data-image-width="462"
                                      data-image-height="693"
                                      :data-href="link()">
-                                <img data-src="/images/default-image.jpg" alt="not found" v-else class="u-expand-resize u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-image u-image-1 lazyload"
+                                <img v-else data-src="/images/default-image.jpg" alt="not found" 
+                                class="u-expand-resize u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-image u-image-1 lazyload"
                                       data-image-width="462"
                                      data-image-height="693"
                                      :data-href="link()">
@@ -76,7 +78,7 @@ parasails.registerComponent('patziPostsItem', {
 
 
   mounted: async function () {
-
+    this.updateStyle(this.$refs.wrapper);
   },
 
   beforeDestroy: function () {
@@ -124,7 +126,28 @@ parasails.registerComponent('patziPostsItem', {
     },
     link() {
       return `/blog/post/${this.post.id}`;
-    }
-
+    },
+/*    updateStyle(wrappers, objData) {
+      if (!_.isArray(wrappers) && wrappers.length < 1) {
+        return;
+      }
+      wrappers.map((wrapper, ind) => {
+        let img = (objData && objData.length >= 1) ? objData[ind].imageSrc : '';
+        img ? $(wrapper).css('backgroundImage', 'url(' + img + ')') : '';
+      });
+    } ,
+              */
+    async updateStyle(wrapper) {
+    /*  const t = `${this.post.valueX}% ${this.post.valueY}%`;
+      let data = {id: this.post.id, position: t, valueX: this.post.valueX, valueY: this.post.valueY};*/
+      wrapper ? wrapper.style.setProperty('object-position', this.post.position) : '';
+      // await io.socket.put(`/api/v1/posts/update-position-img`, data, (data, jwRes) => {
+      //   console.log('Сервер update-position-img ответил кодом ' + jwRes.statusCode + ' и данными: ', data);
+      // });
+      // wrappers.map((wrapper, ind) => {
+      //   let img = (objData && objData.length >= 1) ? objData[ind].imageSrc : '';
+      //   img ? $(wrapper).css('backgroundImage', 'url(' + img + ')') : '';
+      // });
+    },
   }
 });
