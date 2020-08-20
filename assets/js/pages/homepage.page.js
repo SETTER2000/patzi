669b0d1b69fef9f3a7436fb4e4bcfc4431b1dc8a
Александр Александр <lphp@mail.ru>
2020-08-20 21:07:53 +0300

    Ex

--- assets/js/pages/homepage.page.js
+++ assets/js/pages/homepage.page.js
@@ -67,7 +67,7 @@ parasails.registerPage('homepage', {
       return  _.some(this.posts, 'rootPage', true);
     } ,
     getImageUrl(){
-      return   'https://d3a1wbnh2r1l7y.cloudfront.net/root/olga.jpg';
+      return   'https://d3a1wbnh2r1l7y.cloudfront.net/root/root.jpg';
       // return   'https://d3a1wbnh2r1l7y.cloudfront.net/ChesterIMG_0500b.jpg';
     }
 
