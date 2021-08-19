65321b261ea7f6a052df122e052f14a74dab1ac2
SETTER2000 <lphp@mail.ru>
2021-08-20 01:26:42 +0300

    Added sentry verify

--- config/env/production.js
+++ config/env/production.js
@@ -25,6 +25,40 @@
  * https://sailsjs.com/docs/concepts/deployment
  */
 const fs = require('fs');
+
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+  dsn: process.env.PTZ_SENTRY_KEY,
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// const transaction = Sentry.startTransaction({
+//   op: "test",
+//   name: "My First Test Transaction",
+// });
+//
+// setTimeout(() => {
+//   try {
+//     foo();
+//   } catch (e) {
+//     Sentry.captureException(e);
+//   } finally {
+//     transaction.finish();
+//   }
+// }, 99);
+
+
 module.exports = {
 
 
